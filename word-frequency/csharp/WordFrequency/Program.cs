ba438dbb1ba5b68e1cdf24dc4ebd8c4a70b7a1f7
rushayhays <rushayhays@outlook.com>
2022-05-24 16:03:14 -0500

    started files for word frequency

---
+++ word-frequency/csharp/WordFrequency/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordFrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
